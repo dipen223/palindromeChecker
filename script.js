6ace2db90756edd9ace24a2138f932701b2d1454
Dipen <dipenbhandari205@gmail.com>
2024-06-06 10:10:19 +0545

    Add a function to check if a input is empty or not
    when a user clicks the check button without entering an input,then he/she gets an alert message saying : Please input a value

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 const input_text = document.querySelector("#text-input");
 const check_btn  = document.querySelector("#check-btn");
+
+function checkIfEmpty(){
+    if (input_text.value.trim() === ""){
+        window.alert("Please input a value");
+    }
+}
+
+check_btn.onclick = checkIfEmpty;
