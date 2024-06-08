efdcf6c71bf49b6eaaa77845022facb696484043
Dipen <dipenbhandari205@gmail.com>
2024-06-08 09:11:10 +0545

    Add a function to clean the input and convert it into lowercase
    To check if a given phrase is palindrome,we should ignore all the non-alphanumeric characters and spacing ,so regular expressions can be useful in such case

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const input_text = document.querySelector("#text-input");
-const check_btn  = document.querySelector("#check-btn");
+const check_btn = document.querySelector("#check-btn");
 
-function checkIfEmpty(){
-    if (input_text.value.trim() === ""){
+function checkIfEmpty() {
+    if (input_text.value.trim() === "") {
         window.alert("Please input a value");
     }
 }
 
 check_btn.onclick = checkIfEmpty;
+
+function cleanInput(str) {
+    return str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+}
